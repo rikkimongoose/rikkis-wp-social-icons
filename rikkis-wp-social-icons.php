6bb819a16f3c27881a93da99dce65ea90d23de81
Alexander Teut <alexander.a.teut@gmail.com>
2012-07-13 11:50:29 +0300

    ver

--- rikkis-wp-social-icons.php
+++ rikkis-wp-social-icons.php
@@ -3,7 +3,7 @@
 Plugin Name: Rikki's WP Social Icons
 Plugin URI: http://rikkimongoose.ru/projects/rikkis-wp-social-icons/
 Description: Insert social icons to your Wordpress posts.
-Version: 1.0.0
+Version: 1.1.0
 Author: Rikki Mongoose
 Author URI: http://rikkimongoose.ru
 */
